f31d00fd5f9d7fbaa802df55715a7686caea4497
Алексей Дмитриевич Дайнеко <adaineko@technopark.zniis.ru>
2022-09-12 17:33:56 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,34 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Calc/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Calc from './components/Calc.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Calc
+  },
+  data: () => ({
+    counter: 0
+  }),
+  methods: {
+    increase (step, event) {
+      console.log(event)
+      this.counter += step
+    },
+    reset () {
+      this.counter = 0
+    }
   }
 }
+
 </script>
 
 <style lang="scss">
---
+++ src/components/Calc.vue
@@ -0,0 +1,52 @@
+<template>
+<div>
+  <div class="main">
+    <input type="text" v-model.number="op1">
+    <input type="text" v-model.number="op2">
+    = {{ result }}
+  </div>
+  <div class="keyboard">
+    <button @click="sum">+</button>
+    <button @click="div">/</button>
+    <button @click="sub">-</button>
+    <button @click="mult">*</button>
+    <button @click="exponentiation">^</button>
+    <button @click="floor">%</button>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Calc',
+  data: () => ({
+    op1: 0,
+    op2: 0,
+    result: 0
+  }),
+  methods: {
+    sum () {
+      this.result = this.op1 + this.op2
+    },
+    div () {
+      this.result = this.op1 / this.op2
+    },
+    sub () {
+      this.result = this.op1 - this.op2
+    },
+    mult () {
+      this.result = this.op1 * this.op2
+    },
+    exponentiation () {
+      this.result = this.op1 ** this.op2
+    },
+    floor () {
+      this.result = Math.floor( this.op1 / this.op2 )
+    }    
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
